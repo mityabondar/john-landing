4a20a1c6040e82b924c42b3ab71b1c834ad587fe
Dima Bondar <dimamunsky@gmail.com>
2023-02-20 17:56:42 +0300

    first commit

---
+++ dist/assets/scripts.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+  document.querySelectorAll('.js-anchor').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault()
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth',
+          block: 'start'
+        })
+    })
+  })
+})
